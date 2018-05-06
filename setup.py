22a6939aa88439a638620697fd96666c37d306ec
tuxuser <tuxuser360@gmail.com>
2018-05-06 21:30:42 +0200

    Bump version: 1.0.4 → 1.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="xbox-smartglass-core",
-    version="1.0.4",
+    version="1.0.5",
     author="OpenXbox",
     description="A library to interact with the Xbox One gaming console via the SmartGlass protocol.",
     long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
--- xbox/sg/__init__.py
+++ xbox/sg/__init__.py
@@ -3,4 +3,4 @@
 """Top-level package for xbox-smartglass-core-python."""
 
 __author__ = """OpenXbox"""
-__version__ = '1.0.4'
+__version__ = '1.0.5'
