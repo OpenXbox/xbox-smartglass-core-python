15fb02dd56b9a3defe3cab2894863657ae3365b5
tuxuser <462620+tuxuser@users.noreply.github.com>
2018-11-14 15:07:25 +0100

    Update setup.py with support python versions

--- setup.py
+++ setup.py
@@ -27,7 +27,9 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
     ],
     install_requires=[
         'xbox-webapi>=1.1.2',
