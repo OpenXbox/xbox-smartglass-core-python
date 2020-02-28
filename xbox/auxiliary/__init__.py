d9376b86bdb0ed3a92bc4b2ac39cde47e6005c18
tuxuser <462620+tuxuser@users.noreply.github.com>
2020-02-28 23:54:36 +0100

    Merge metadata into common __author__ and __version__ info

--- xbox/auxiliary/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for xbox-smartglass-auxiliary-python."""
-
-__author__ = """OpenXbox"""
-__version__ = '0.9.2'
--- xbox/sg/__init__.py
+++ xbox/sg/__init__.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for xbox-smartglass-core-python."""
+"""
+Meta package for xbox-smartglass-core-python.
+Version and author information.
+"""
 
 __author__ = """OpenXbox"""
 __version__ = '1.0.12'
