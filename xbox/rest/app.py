8a8dd5407298d958211c74b614258d49d7a3e1d2
tuxuser <462620+tuxuser@users.noreply.github.com>
2020-10-31 17:01:04 +0100

    REST: Add __main__ handler for xbox.rest.app

--- xbox/rest/app.py
+++ xbox/rest/app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+import uvicorn
 
 from .api import api_router
 
 app = FastAPI(title='SmartGlass REST server')
 app.include_router(api_router)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=5557)
